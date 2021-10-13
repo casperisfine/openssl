8bdb729c0db8f27831525fb35b1025b3a0baf1cc
Jean Boussier <jean.boussier@gmail.com>
2021-10-13 17:19:09 +0200

    Verify compaction references

--- test/openssl/utils.rb
+++ test/openssl/utils.rb
@@ -29,6 +29,12 @@ if ENV["OSSL_MDEBUG"] == "1"
   end
 end
 
+if GC.respond_to?(:verify_compaction_references)
+  # This method was added in Ruby 3.0.0. Calling it this way asks the GC to
+  # move objects around, helping to find object movement bugs.
+  GC.verify_compaction_references(double_heap: true, toward: :empty)
+end
+
 require "test/unit"
 require "tempfile"
 require "socket"
